33eeac048a525ab851fd26209e43307d38445e64
Janne Juote <janne.henrik.juote@gmail.com>
2026-04-22 11:07:03 +0300

    Add print styles for compact 2-page PDF output

--- push2.bat
+++ push2.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=C:\Program Files\Git\bin;C:\Program Files\GitHub CLI;%PATH%
 git add .
-git commit -m "Add GitHub Pages deployment workflow"
+git commit -m "Add print styles for compact 2-page PDF output"
 git push origin master
